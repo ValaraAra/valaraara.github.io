28341364e874ccf874192390f066f0f8cd94a470
Aubrey <fivedarkops@gmail.com>
2024-09-01 23:48:43 -0700

    added slight hover scaling for projects

--- src/components/ProjectGridItem.vue
+++ src/components/ProjectGridItem.vue
@@ -7,8 +7,8 @@ const props = defineProps({
 </script>
 
 <template>
-	<div class="flex gap-4 rounded-lg border dark:border-surface-700 w-full h-56 bg-surface-50 dark:bg-surface-900 overflow-hidden">
-		<div class="w-5/12 relative">
+	<div class="flex gap-4 rounded-lg border dark:border-surface-700 w-full h-56 bg-surface-50 dark:bg-surface-900 overflow-hidden hover:scale-[1.01]">
+		<div class="w-5/12 relative ">
 			<img class="rounded-l-lg object-cover shrink-0 size-full" :src="props.project.image" alt="project image" />
 			<div class="absolute size-full top-0 left-0 bg-gradient-to-l from-surface-50 dark:from-surface-900 dark:to-60% to-transparent"></div>
 		</div>
