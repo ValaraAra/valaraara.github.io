89564afd62cef27ce62bb63ef1dd45bc9db976b3
Aubrey <fivedarkops@gmail.com>
2025-09-15 02:37:11 -0700

    updated email

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -8,7 +8,7 @@
 		<p class="dark:text-surface-400">If you'd like to get in touch, feel free to reach out!</p>
 		<div class="flex flex-col sm:flex-row gap-4 mt-4 justify-between place-items-center place-content-center">
 			<IconTextLink icon="pi-github text-surface-900 dark:text-surface-0" text="Github" href="https://github.com/valaraara" />
-			<IconTextLink icon="pi-envelope text-surface-0" text="kochaub@oregonstate.edu" href="mailto:kochaub@oregonstate.edu" />
+			<IconTextLink icon="pi-envelope text-surface-0" text="contact@aubreykoch.com" href="mailto:contact@aubreykoch.com" />
 			<IconTextLink icon="pi-linkedin text-blue-600" text="LinkedIn" href="https://www.linkedin.com/in/aubreykoch/" />
 		</div>
 	</div>
