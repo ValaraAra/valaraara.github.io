8d71bb1d0d78c872a66bbfd2b776503936a6babd
Aubrey <fivedarkops@gmail.com>
2025-09-23 20:40:28 -0700

    minor fix

--- src/components/ProjectGrid.vue
+++ src/components/ProjectGrid.vue
@@ -34,7 +34,7 @@ const projects = [
 	<div class="flex flex-col gap-6" id="projects">
 		<h1 class="text-3xl font-semibold">Featured Projects</h1>
 		<div class="flex flex-col gap-6">
-			<ProjectGridItem v-for="(project, index) in projects" :project="project" />
+			<ProjectGridItem v-for="(project, index) in projects" :project="project" :key="project.title" />
 		</div>
 	</div>
 </template>
