12831682492bab0726fbb330ad38ceebb5e7ce4f
Aubrey <fivedarkops@gmail.com>
2025-10-07 23:00:17 -0700

    removed gravatar image src

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -6,7 +6,7 @@ import IconTextLink from './IconTextLink.vue';
 
 <template>
 	<div class="flex flex-col items-center md:flex-row gap-8 justify-start mt-12 w-full">
-		<Avatar class="border-4 border-primary size-32" image="https://gravatar.com/avatar/32f1707bf470bc71a1dee6f60ee5cf96?size=256" shape="circle" size="xlarge" aria-label="avatar image" />
+		<Avatar class="border-4 border-primary size-32" image="/images/profile.png" shape="circle" size="xlarge" aria-label="avatar image" />
 		<div class="flex flex-col gap-5 max-w-[30rem] text-left">
 			<p class="text-3xl text-surface-400">
 				<span class="inline-block font-semibold text-surface-200">Hey, I'm Aubrey.</span>
