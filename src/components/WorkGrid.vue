de750cc565b467cc12570a8a4025e87f7eb118db
Aubrey <valara.official@gmail.com>
2026-03-07 01:21:35 -0800

    updated work description

--- src/components/WorkGrid.vue
+++ src/components/WorkGrid.vue
@@ -5,8 +5,8 @@ const projects = [
 	{
 		image: '/images/work.png',
 		title: 'Oregon State University',
-		text: 'As a Web Developer for the College of Engineering, I implement new pages, features, and redesigns. This work is done with a high level of collaboration with the design and marketing teams.',
-		chips: ['JS', 'HTML/CSS', 'Bootstrap', 'Drupal', 'Azure DevOps'],
+		text: 'As a Web Developer for the College of Engineering, I implement new pages, features, and redesigns alongside our design and marketing teams. Currently project lead for accessibility.',
+		chips: ['JS', 'HTML/CSS', 'Bootstrap', 'Drupal', 'WCAG'],
 		link: 'https://engineering.oregonstate.edu/'
 	},
 ]
