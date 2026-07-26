446df695b0cc25b63aa134b0bf96b6dbd62d7bd3
Aubrey <valara.official@gmail.com>
2026-07-26 01:27:27 -0700

    reimplement starry background with canvas

--- src/components/AppBackground.vue
+++ src/components/AppBackground.vue
@@ -1,54 +1,127 @@
 <script setup>
-	import { ref, onMounted } from 'vue'
+	import { ref, onMounted, onUnmounted } from 'vue'
 	
-	const stars = ref([])
+	const starryCanvasRef = ref(null)
+	let animationFrameID, context, starSprite
+	
+	const stars = []
+	const starCount = 200
 
-	onMounted(() => {
-		for (let i = 0; i < 200; i++) {
-			stars.value.push({
-				id: i,
-				left: Math.random() * 100,
-				top: Math.random() * 100,
-				duration: 2 + (Math.random() * 3)
+	const points = 4
+	const inset = 0.5
+	const radius = 3
+
+	function resizeCanvas() {
+		const canvas = starryCanvasRef.value
+		
+		const oldWidth = canvas.width
+		const oldHeight = canvas.height
+
+		canvas.width = canvas.clientWidth
+		canvas.height = canvas.clientHeight
+
+		if (oldWidth && oldHeight) {
+			const widthRatio = canvas.width / oldWidth
+			const heightRatio = canvas.height / oldHeight
+
+			for (const star of stars) {
+				star.x *= widthRatio
+				star.y *= heightRatio
+			}
+		}
+	}
+
+	function generateStars() {
+		const canvas = starryCanvasRef.value
+
+		stars.length = 0
+		for (let i = 0; i < starCount; i++) {
+			stars.push({
+				x: Math.random() * canvas.width,
+				y: Math.random() * canvas.height,
+				speed: Math.random() * 1.5 + 0.5,
+				phase: Math.random() * Math.PI * 2
 			})
 		}
+	}
+
+	// Function adapted from Dennis S. at https://stackoverflow.com/a/45140101
+	function drawStar(context, x, y) {
+		context.save();
+		context.beginPath();
+		context.translate(x, y);
+		context.moveTo(0, 0 - radius);
+		for (let i = 0; i < points; i++) {
+			context.rotate(Math.PI / points);
+			context.lineTo(0, 0 - (radius * inset));
+			context.rotate(Math.PI / points);
+			context.lineTo(0, 0 - radius);
+		}
+		context.closePath();
+		context.fill();
+		context.restore();
+	}
+
+	function drawStarSprite() {
+		const spriteCanvas = document.createElement('canvas')
+
+		const size = radius * 2 + 2
+		spriteCanvas.width = size
+		spriteCanvas.height = size
+
+		const spriteContext = spriteCanvas.getContext('2d')
+		spriteContext.fillStyle = 'white'
+		drawStar(spriteContext, size / 2, size / 2)
+
+		return spriteCanvas
+	}
+
+	function animationStep(timestamp) {
+		const canvas = starryCanvasRef.value
+		context.clearRect(0, 0, canvas.width, canvas.height)
+
+		const halfSize = starSprite.width / 2
+
+		for (const star of stars) {
+			const alpha = 0.5 + 0.5 * Math.sin(star.phase + (timestamp / 1000) * star.speed)
+			context.globalAlpha = alpha
+
+			context.drawImage(starSprite, star.x - halfSize, star.y - halfSize)
+		}
+
+		animationFrameID = requestAnimationFrame(animationStep)
+	}
+
+	onMounted(() => {
+		starSprite = drawStarSprite()
+
+		resizeCanvas()
+		generateStars()
+
+		context = starryCanvasRef.value.getContext('2d')
+		context.imageSmoothingEnabled = false
+
+		animationFrameID = requestAnimationFrame(animationStep)
+		window.addEventListener('resize', resizeCanvas)
+	})
+
+	onUnmounted(() => {
+		cancelAnimationFrame(animationFrameID)
+		window.removeEventListener('resize', resizeCanvas)
 	})
 </script>
 
 <template>
-	<div class="starry-background">
-		<div class="star" v-for="star in stars" :key="star.id" :style="{
-				left: star.left + '%',
-				top: star.top + '%',
-				animationDuration: star.duration + 's'
-			}"
-		></div>
-	</div>
+	<canvas ref="starryCanvasRef" class="starry-background"></canvas>
 </template>
 
 <style scoped>
 	.starry-background {
 		position: fixed;
-		top: 0;
-		left: 0;
+		inset: 0;
 		width: 100%;
 		height: 100%;
 		z-index: -1;
 		background: linear-gradient(to bottom, #221b35, #1d162f, #16102a, #0f0b22, #090714);
-		pointer-events: none;
-	}
-
-	.star {
-		position: absolute;
-		width: 4px;
-		height: 4px;
-		background: white;
-		clip-path: polygon(50% 0%, 60% 40%, 100% 50%, 60% 60%, 50% 100%, 40% 60%, 0% 50%, 40% 40%);
-		animation: twinkle infinite ease-in-out;
-	}
-
-	@keyframes twinkle {
-		0%, 100% { opacity: 0.3; transform: scale(1); }
-		50% { opacity: 1; transform: scale(1.3); }
 	}
 </style>
